15578d334a7c5d6938e059b3217eae9b9dad7cc3
Pashko Ostap <pashko.ostap@gmail.com>
2019-08-03 19:54:03 +0300

    added vehicles dropdown

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const dbUrl = "mongodb://localhost:27017/buses-lviv-region__19-05-2019";
+const dbUrl = 'mongodb://localhost:27017/buses-lviv-region__02-08-2019';
 const baseApiUrl =
-  "http://82.207.107.126:13541/tms/loda/api/389C2151-C5E9-4D47-8B94-5F643CA0AFEB/";
+  'http://82.207.107.126:13541/tms/loda/api/389C2151-C5E9-4D47-8B94-5F643CA0AFEB/';
 const config = {
   dbUrl,
   getStopsUrl: `${baseApiUrl}/GetStops`,
--- routes/routes.js
+++ routes/routes.js
@@ -1,33 +1,33 @@
-const mongoose = require("mongoose");
-const Router = require("koa-router");
+const mongoose = require('mongoose');
+const Router = require('koa-router');
 const routes = new Router();
-const { Route } = require("../schemas/Route");
-const { RouteStop } = require("../schemas/RouteStop");
-const { RouteStopSchedule } = require("../schemas/RouteStopSchedule");
-const { RouteVehicle } = require("../schemas/RouteVehicle");
+const { Route } = require('../schemas/Route');
+const { RouteStop } = require('../schemas/RouteStop');
+const { RouteStopSchedule } = require('../schemas/RouteStopSchedule');
+const { RouteVehicle } = require('../schemas/RouteVehicle');
 
 routes
-  .get("/", async (ctx, next) => {
-    const routes = await Route.find({}, "-stops -routeVehicles");
+  .get('/', async (ctx, next) => {
+    const routes = await Route.find({}, '-stops -routeVehicles');
 
     ctx.body = routes;
   })
-  .get("/:id", async (ctx, next) => {
+  .get('/:id', async (ctx, next) => {
     const { routeId, date } = ctx.query;
 
-    const route = await Route.findOne({ Id: parseInt(routeId) }, "Name -_id")
+    const route = await Route.findOne({ Id: parseInt(routeId) }, 'Name -_id')
       .populate({
-        path: "stops",
+        path: 'stops',
         model: RouteStop,
-        select: "Name X Y -_id",
+        select: 'Name X Y -_id',
         populate: {
-          path: "schedule",
+          path: 'schedule',
           model: RouteStopSchedule,
-          select: "TimeToPoint -_id"
+          select: 'TimeToPoint -_id'
         }
       })
       .populate({
-        path: "routeVehicles",
+        path: 'routeVehicles',
         model: RouteVehicle,
         match: {
           created: {
@@ -35,7 +35,7 @@ routes
             $lt: parseInt(date) + 24 * 60 * 60 * 1000
           }
         },
-        select: "X Y created VehicleName"
+        select: 'X Y created VehicleName'
       });
     ctx.body = route;
   });
